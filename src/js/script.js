feaaacf309cda9c494613489491f74fddf26967b
Jerry Wang <jerrywang0129@hotmail.com>
2020-12-17 21:57:44 -0500

    test

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,70 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    let connexion = new MovieDB();
+    connexion.requeteDernierFilm();
+
+})
+
+
+class MovieDB {
+
+    constructor() {
+        console.log("New MovieDB()");
+        this.apiKey = "0c479a8fa3849ea10fb0137114f0de03";
+        this.lang = "fr-CA";
+        this.baseUrl = "https://api.themoviedb.org/3/";
+        this.imgPath = "https://image.tmdb.org/t/p/";
+        this.nbFilm = 8;
+    }
+
+    requeteDernierFilm() {
+        let requete = new XMLHttpRequest();
+        requete.addEventListener("loadend", this.retourDernierFilm.bind(this));
+        //requete.open("GET", "https://api.themoviedb.org/3/movie/now_playing?api_key=0c479a8fa3849ea10fb0137114f0de03&language=fr-CA&page=1");
+        requete.open("GET", this.baseUrl + "movie/now_playing?api_key=" + this.apiKey + "&language=" + this.lang + "&page=1");
+        requete.send();
+    }
+
+    retourDernierFilm(event) {
+        console.log('retourDernierFilm')
+        let target = event.currentTarget;
+        let data = JSON.parse(target.responseText).results;
+
+
+        this.afficherDernierFilm(data);
+
+    }
+
+    afficherDernierFilm(data) {
+        console.log('affiche')
+
+        let section = document.querySelector('.liste-films');
+        console.log(section)
+
+        for (let i = 0; i < this.nbFilm; i++) {
+            let article = document.querySelector('.template .film').cloneNode(true);
+            article.querySelector('h2').innerHTML = data[i].title;
+            /*if(data[i].overview !== ""){
+                article.querySelector('.description').innerHTML = data[i].overview;
+            }
+            else
+            {
+                article.querySelector('.description').innerHTML = "Aucune description disponible"
+            }*/
+
+            article.querySelector('.description').innerHTML = data[i].overview;
+            let image = article.querySelector('img');
+            image.src = this.imgPath + "w300" + data[i].poster_path;
+
+            section.appendChild(article);
+        }
+    }
+}
+
+
+
+
+/*
 document.addEventListener("DOMContentLoaded", function(){
 
     let connexion = new MovieDB();
@@ -67,4 +134,4 @@ class MovieDB{
     }
 
 
-}
+}*/
